de37ee2fba2b7f647196b0c492bde7a06866ce19
Steve Kondik <shade@chemlab.org>
2013-08-16 14:55:31 -0700

    fsck_msdos: Static-ify
     * Split out main() and libraryify so we can use in recovery.
    
    Change-Id: I1b35d7f52f536145c3c465030391f86100fa2ac0

--- Android.mk
+++ Android.mk
@@ -1,14 +1,22 @@
 LOCAL_PATH := $(call my-dir)
 
+common_src_files := fragment.c fatcache.c boot.c check.c dir.c fat.c main.c
+
+common_cflags := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
+
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := fragment.c fatcache.c boot.c check.c dir.c fat.c main.c
-
+LOCAL_SRC_FILES := $(common_src_files)
+LOCAL_CFLAGS := $(common_cflags)
 LOCAL_C_INCLUDES := external/fsck_msdos/
+LOCAL_MODULE := libfsck_msdos
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_STATIC_LIBRARY)
 
-LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
-
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := fsck_msdos.c
+LOCAL_CFLAGS := $(common_cflags)
+LOCAL_C_INCLUDES := external/fsck_msdos/
 LOCAL_MODULE := fsck_msdos
 LOCAL_MODULE_TAGS :=
-LOCAL_SYSTEM_SHARED_LIBRARIES := libc  libcutils
-
+LOCAL_STATIC_LIBRARIES := libfsck_msdos
 include $(BUILD_EXECUTABLE)
