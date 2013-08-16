392772ece2c5d641e1a47c51fd7736daf3c74427
Steve Kondik <shade@chemlab.org>
2013-08-16 14:55:31 -0700

    fsck_msdos: Static-ify
     * Split out main() and libraryify so we can use in recovery.
    
    Change-Id: I1b35d7f52f536145c3c465030391f86100fa2ac0

--- Android.mk
+++ Android.mk
@@ -2,18 +2,24 @@ ifneq ($(BOARD_USES_CUSTOM_FSCK_MSDOS),true)
 
 LOCAL_PATH := $(call my-dir)
 
+common_src_files := common_cflags := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
+
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := boot.c check.c dir.c fat.c main.c
-
+LOCAL_CFLAGS := $(common_cflags)
 LOCAL_C_INCLUDES := external/fsck_msdos/
+LOCAL_MODULE := libfsck_msdos
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_STATIC_LIBRARY)
 
-LOCAL_CFLAGS := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
-
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := fsck_msdos.c
+LOCAL_CFLAGS := $(common_cflags)
+LOCAL_C_INCLUDES := external/fsck_msdos/
 LOCAL_MODULE := fsck_msdos
 LOCAL_MODULE_TAGS :=
+LOCAL_STATIC_LIBRARIES := libfsck_msdos
 LOCAL_SYSTEM_SHARED_LIBRARIES := libc
-
 include $(BUILD_EXECUTABLE)
 
 endif
---
+++ fsck_msdos.c
@@ -0,0 +1,7 @@
+
+extern int fsck_msdos_main(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+    return fsck_msdos_main(argc, argv);
+}
