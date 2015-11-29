9b4dce4f33e75ff7d73d0546b6ef0c58b082acfb
Michael Bestas <mikeioannina@gmail.com>
2015-11-29 23:04:41 +0200

    fsck_msdos: Rework build configuration
    This partially reverts commit 5640e941d89a86e1b34165b1627f3002240de750.
    
    Change-Id: Ia491899097148fcf044163b58f9f62d0889840fd

--- Android.mk
+++ Android.mk
@@ -6,17 +6,17 @@ common_cflags := -O2 -g -W -Wall -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(common_src_files)
-LOCAL_CFLAGS := $(common_cflags)
+LOCAL_CFLAGS := $(common_cflags) -Dmain=fsck_msdos_main
 LOCAL_C_INCLUDES := external/fsck_msdos/
-LOCAL_MODULE := libfsck_msdos
+LOCAL_MODULE := libfsck_msdos_static
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := fsck_msdos.c
+LOCAL_SRC_FILES := $(common_src_files)
 LOCAL_CFLAGS := $(common_cflags)
 LOCAL_C_INCLUDES := external/fsck_msdos/
 LOCAL_MODULE := fsck_msdos
 LOCAL_MODULE_TAGS :=
-LOCAL_STATIC_LIBRARIES := libfsck_msdos liblog
+LOCAL_STATIC_LIBRARIES := liblog
 include $(BUILD_EXECUTABLE)
--- fsck_msdos.c
+++
@@ -1,6 +0,0 @@
-#include "fsck_msdos.h"
-
-int main(int argc, char** argv)
-{
-    return fsck_msdos_main(argc, argv);
-}
